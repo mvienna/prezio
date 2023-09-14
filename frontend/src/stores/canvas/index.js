da67e4123ee9fb8500db349bc03b58b8cf54e63c
inagamov <inagamov.tyoma@gmail.com>
2023-09-14 23:38:44 +0300

    temp disable rendering slide preview

--- frontend/src/stores/canvas/index.js
+++ frontend/src/stores/canvas/index.js
@@ -268,7 +268,7 @@ export const useCanvasStore = defineStore("canvas", {
       /*
        * compute preview
        */
-      slide.value.preview = this.canvas.toDataURL("image/png");
+      // slide.value.preview = this.canvas.toDataURL("image/png");
     },
 
     /*
