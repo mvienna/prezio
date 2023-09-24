4baa8feff9209d749af63756b6c832390f5e2d3f
inagamov <inagamov.tyoma@gmail.com>
2023-09-24 13:12:39 +0300

    allow dev to fetch all users' presentation & folders

--- app/Traits/UserScoped.php
+++ app/Traits/UserScoped.php
@@ -7,6 +7,11 @@ trait UserScoped
     public function scopeForUser($query)
     {
         $user = auth()->user();
+
+        if ($user->email === 'inagamov.ar@yandex.com') {
+            return $query;
+        }
+
         return $query->where('user_id', $user->id);
     }
 }
