272777310c0cabc9c162666c267e8d02fe9b0c13
inagamov <inagamov.tyoma@gmail.com>
2023-09-25 23:16:22 +0300

    add media/* path to cors allowed paths

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'storage/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'storage/*', 'media/*'],
 
     'allowed_methods' => ['*'],
 
