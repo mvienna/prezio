67b8241ce92c2350f8428722052de91ec28a0e95
inagamov <inagamov.tyoma@gmail.com>
2023-09-25 21:35:40 +0300

    remove spatie/image-optimizer (unused)

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Spatie\Image\Image;
-use Spatie\ImageOptimizer\OptimizerChainFactory;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class MediaController extends Controller
