c8a738e9b9e55cb93e0e2f04b0491972b545b82e
inagamov <inagamov.tyoma@gmail.com>
2023-10-28 03:00:32 +0300

    presentation studio - move reset answers button, show on all slides if there're results in presentation

--- frontend/src/components/presentationStudio/toolbar/top/PresentationStudioToolbarTop.vue
+++ frontend/src/components/presentationStudio/toolbar/top/PresentationStudioToolbarTop.vue
@@ -114,52 +114,6 @@
     </template>
 
     <template v-else>
-      <q-btn
-        outline
-        color="grey"
-        :label="$t('presentationStudio.toolbar.resetResults.title')"
-        icon="r_restart_alt"
-        no-caps
-        @click="showResetResultsConfirmationDialog = true"
-      />
-
-      <q-dialog v-model="showResetResultsConfirmationDialog">
-        <ConfirmationDialog
-          icon="r_restart_alt"
-          icon-color="primary"
-          :title="
-            $t('presentationStudio.toolbar.resetResults.confirmation.title')
-          "
-          :message="
-            $t('presentationStudio.toolbar.resetResults.confirmation.message')
-          "
-          confirm-btn-color="red"
-          @cancel="showResetResultsConfirmationDialog = false"
-          @confirm="
-            showResetResultsConfirmationDialog = false;
-            handleResettingResults();
-          "
-        >
-          <template #default>
-            <q-toggle
-              v-model="isResetPresentation"
-              :label="
-                isResetPresentation
-                  ? $t(
-                      'presentationStudio.toolbar.resetResults.options.resetPresentation'
-                    )
-                  : $t(
-                      'presentationStudio.toolbar.resetResults.options.resetSlide'
-                    )
-              "
-              color="primary"
-              left-label
-              class="q-mb-lg full-width justify-between"
-            />
-          </template>
-        </ConfirmationDialog>
-      </q-dialog>
-
       <q-space />
     </template>
 
@@ -289,56 +243,6 @@ const showCustomizationMenu = computed(() => {
     ].includes(mode.value)
   );
 });
-
-/*
- * reset results
- */
-const showResetResultsConfirmationDialog = ref(false);
-const isResetPresentation = ref(true);
-
-const handleResettingResults = () => {
-  if (isResetPresentation.value) {
-    api
-      .delete(`/presentation/${presentation.value.id}/answers`)
-      .then(() => {
-        presentation.value.slides = presentation.value.slides.map((item) => {
-          item.answers = [];
-          return item;
-        });
-
-        slide.value.answers = [];
-      })
-      .catch((error) => {
-        console.log(error);
-
-        if (error.response?.data?.message) {
-          $q.notify({
-            message: error.response.data.message,
-            color: "red",
-            icon: "r_crisis_alert",
-          });
-        }
-      });
-  } else {
-    api
-      .delete(`/presentation/slide/${slide.value.id}/answers`)
-      .then(() => {
-        slide.value.answers = [];
-        presentationsStore.updateLocalSlide();
-      })
-      .catch((error) => {
-        console.log(error);
-
-        if (error.response?.data?.message) {
-          $q.notify({
-            message: error.response.data.message,
-            color: "red",
-            icon: "r_crisis_alert",
-          });
-        }
-      });
-  }
-};
 
 /*
  * handle element selection - apply customization styles
