04a68073fd4d86e9e9fb5febbabdf2a16e8f6445
inagamov <inagamov.tyoma@gmail.com>
2023-12-10 15:31:53 +0300

    leaderboard (participant view) - hide champion cup if no results

--- frontend/src/components/presentationRoom/participant/PresentationRoomParticipantLeaderboard.vue
+++ frontend/src/components/presentationRoom/participant/PresentationRoomParticipantLeaderboard.vue
@@ -14,119 +14,123 @@
       style="z-index: 2; overflow-y: scroll; height: calc(100vh - 96px)"
       class="scroll--hidden q-pb-lg"
     >
-      <div class="row justify-center">
-        <q-img src="/assets/images/champion.svg" class="champion_cup" />
-      </div>
-
-      <div
-        class="text-center"
-        :class="`${
-          averageBackgroundBrightness >= backgroundBrightnessThreshold
-            ? 'text-black'
-            : 'text-white'
-        }`"
-      >
-        <div class="text-h6 text-semibold">
-          {{ $t("presentationRoom.leaderboard.title") }}
+      <template v-if="results?.length">
+        <div class="row justify-center">
+          <q-img src="/assets/images/champion.svg" class="champion_cup" />
         </div>
 
-        <div class="q-mt-sm">
-          {{ $t("presentationRoom.leaderboard.youAreIn") }}
+        <div
+          class="text-center"
+          :class="`${
+            averageBackgroundBrightness >= backgroundBrightnessThreshold
+              ? 'text-black'
+              : 'text-white'
+          }`"
+        >
+          <div class="text-h6 text-semibold">
+            {{ $t("presentationRoom.leaderboard.title") }}
+          </div>
 
-          <span class="text-semibold">{{ participantResults?.place }}</span>
-          {{ $t("presentationRoom.leaderboard.place") }}
-          {{ $t("presentationRoom.leaderboard.outOf") }}
-          <span class="text-semibold">{{ results.length }}</span>
-          {{ $t("presentationRoom.leaderboard.players") }}
+          <div class="q-mt-sm">
+            {{ $t("presentationRoom.leaderboard.youAreIn") }}
+
+            <span class="text-semibold">{{ participantResults?.place }}</span>
+            {{ $t("presentationRoom.leaderboard.place") }}
+            {{ $t("presentationRoom.leaderboard.outOf") }}
+            <span class="text-semibold">{{ results.length }}</span>
+            {{ $t("presentationRoom.leaderboard.players") }}
+          </div>
+
+          <div>
+            {{ $t("presentationRoom.leaderboard.youScored") }}
+
+            <span class="text-semibold">
+              {{ participantResults?.data?.score }}
+            </span>
+            {{ $t("presentationRoom.leaderboard.points") }}
+          </div>
         </div>
 
-        <div>
-          {{ $t("presentationRoom.leaderboard.youScored") }}
+        <div class="row no-wrap justify-center q-pa-lg">
+          <div class="column no-wrap q-gutter-md full-width q-pt-md">
+            <div
+              v-for="(result, index) in results"
+              :key="result?.participant?.id"
+              class="row no-wrap full-width"
+            >
+              <div class="column justify-center">
+                <q-btn
+                  round
+                  flat
+                  style="border-radius: 50%; backdrop-filter: blur(4px)"
+                  :class="
+                    averageBackgroundBrightness >= backgroundBrightnessThreshold
+                      ? 'text-black'
+                      : 'text-white'
+                  "
+                  :style="
+                    averageBackgroundBrightness >= backgroundBrightnessThreshold
+                      ? `background: rgba(255, 255, 255, 0.5)`
+                      : 'background: rgba(0, 0, 0, 0.5)'
+                  "
+                  class="q-mr-md"
+                  size="12px"
+                >
+                  {{ index + 1 }}
+                </q-btn>
+              </div>
 
-          <span class="text-semibold">
-            {{ participantResults?.data?.score }}
-          </span>
-          {{ $t("presentationRoom.leaderboard.points") }}
-        </div>
-      </div>
-
-      <div v-if="results?.length" class="row no-wrap justify-center q-pa-lg">
-        <div class="column no-wrap q-gutter-md full-width q-pt-md">
-          <div
-            v-for="(result, index) in results"
-            :key="result?.participant?.id"
-            class="row no-wrap full-width"
-          >
-            <div class="column justify-center">
-              <q-btn
-                round
+              <q-card
                 flat
-                style="border-radius: 50%; backdrop-filter: blur(4px)"
+                style="
+                  border-radius: 16px !important;
+                  overflow: hidden;
+                  width: 100%;
+                "
+                class="bg-white"
+              >
+                <div
+                  class="absolute-left"
+                  style="height: 100%; border-radius: 0"
+                  :style="`background: rgba(${Object.values(
+                    colors.textToRgb(result.participantData?.color || '#FFFFFF')
+                  ).join(',')}, 0.7); width: ${
+                    (result.score * 100) / maxScore
+                  }%`"
+                ></div>
+
+                <q-card-section
+                  class="row no-wrap items-center"
+                  :class="$q.screen.lt.lg ? 'q-pa-sm' : 'q-pa-md'"
+                >
+                  <div
+                    class="text-semibold ellipsis text-no-wrap"
+                    style="font-size: 1em"
+                  >
+                    {{
+                      result.participantData?.avatar +
+                      " " +
+                      result.participantData?.name
+                    }}
+                  </div>
+                </q-card-section>
+              </q-card>
+
+              <div
+                class="text-no-wrap column justify-center q-ml-md"
                 :class="
                   averageBackgroundBrightness >= backgroundBrightnessThreshold
                     ? 'text-black'
                     : 'text-white'
                 "
-                :style="
-                  averageBackgroundBrightness >= backgroundBrightnessThreshold
-                    ? `background: rgba(255, 255, 255, 0.5)`
-                    : 'background: rgba(0, 0, 0, 0.5)'
-                "
-                class="q-mr-md"
-                size="12px"
               >
-                {{ index + 1 }}
-              </q-btn>
-            </div>
-
-            <q-card
-              flat
-              style="
-                border-radius: 16px !important;
-                overflow: hidden;
-                width: 100%;
-              "
-              class="bg-white"
-            >
-              <div
-                class="absolute-left"
-                style="height: 100%; border-radius: 0"
-                :style="`background: rgba(${Object.values(
-                  colors.textToRgb(result.participantData?.color || '#FFFFFF')
-                ).join(',')}, 0.7); width: ${(result.score * 100) / maxScore}%`"
-              ></div>
-
-              <q-card-section
-                class="row no-wrap items-center"
-                :class="$q.screen.lt.lg ? 'q-pa-sm' : 'q-pa-md'"
-              >
-                <div
-                  class="text-semibold ellipsis text-no-wrap"
-                  style="font-size: 1em"
-                >
-                  {{
-                    result.participantData?.avatar +
-                    " " +
-                    result.participantData?.name
-                  }}
-                </div>
-              </q-card-section>
-            </q-card>
-
-            <div
-              class="text-no-wrap column justify-center q-ml-md"
-              :class="
-                averageBackgroundBrightness >= backgroundBrightnessThreshold
-                  ? 'text-black'
-                  : 'text-white'
-              "
-            >
-              {{ result.score }}
-              {{ $t("presentationRoom.leaderboard.p") }}
+                {{ result.score }}
+                {{ $t("presentationRoom.leaderboard.p") }}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </template>
 
       <div
         v-else
