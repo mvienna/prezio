c7504e6810f3cf1a35c3b881717b6af21148b1ac
inagamov <inagamov.tyoma@gmail.com>
2023-12-12 18:48:21 +0300

    profanity - input value to lower case

--- frontend/src/components/presentationRoom/participant/forms/submissions/PresentationRoomParticipantFormsSubmissionsWordCloud.vue
+++ frontend/src/components/presentationRoom/participant/forms/submissions/PresentationRoomParticipantFormsSubmissionsWordCloud.vue
@@ -244,7 +244,7 @@ const answerRules = [
   (val) =>
     (slideSettings.value?.filterProfanity
       ? !russianProfanityWords.filter((word) => {
-          return val?.replaceAll(/\s/g, "")?.includes(word);
+          return val?.replaceAll(/\s/g, "").toLowerCase()?.includes(word);
         }).length
       : true) || t("presentationRoom.answers.errors.profanity"),
 ];
