dd378578d743bcbbd0066e7618e68df170ed36ab
inagamov <inagamov.tyoma@gmail.com>
2023-12-14 16:35:54 +0300

    barchart - remove bar value text on update

--- frontend/src/components/presentation/addons/PresentationAddonsBarChart.vue
+++ frontend/src/components/presentation/addons/PresentationAddonsBarChart.vue
@@ -217,6 +217,7 @@ const update = (isOnLoad = false) => {
     );
 
   svg.value.selectAll("image").remove();
+  svg.value.selectAll("text").remove();
 
   x.value = d3.scaleBand().range([0, width.value]).padding(0.2);
   xAxis.value = svg.value
