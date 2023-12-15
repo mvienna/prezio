0c50d727cfed96c7bb341bcbd8e58bc870fef8e0
inagamov <inagamov.tyoma@gmail.com>
2023-12-15 03:54:41 +0300

    websockets & broadcasting configs update

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -29,7 +29,6 @@ return [
     */
 
     'connections' => [
-
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
@@ -37,7 +36,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
+                'host' => env('PUSHER_HOST'),
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
@@ -65,7 +64,5 @@ return [
         'null' => [
             'driver' => 'null',
         ],
-
     ],
-
 ];
