ee4fa1f53eb4e660a4124534bdbfb57dd2098a0a
inagamov <inagamov.tyoma@gmail.com>
2024-01-08 10:14:21 +0300

    feedback button style update

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -26,16 +26,13 @@
           v-if="feedbackFromUrl"
           :href="feedbackFromUrl"
           target="_blank"
-          class="q-mr-md"
-          unelevated
+          flat
           no-caps
-          color="primary"
-        >
-          <div class="shiny_text q-mr-sm">
-            {{ $t("mainLayout.header.feedbackForm.title") }}
-          </div>
-          <q-icon name="r_open_in_new" size="20px" />
-        </q-btn>
+          text-color="black"
+          icon="o_reviews"
+          :label="$t('mainLayout.header.feedbackForm.title')"
+        />
+        <q-separator vertical class="q-my-sm q-mx-md" />
 
         <!-- notifications -->
         <q-btn
@@ -89,60 +86,3 @@ const feedbackFromUrl = computed(() => {
   return process.env.FEEDBACK_FORM_URL;
 });
 </script>
-
-<style scoped lang="scss">
-.shiny_text {
-  color: #fff;
-  background: linear-gradient(to right, $grey-4 0, $white 10%, $grey-4 20%);
-  background-position: 0;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  animation: shine 3s infinite linear;
-  animation-fill-mode: forwards;
-  -webkit-text-size-adjust: none;
-}
-@keyframes shine {
-  0% {
-    background-position: 0;
-  }
-  60% {
-    background-position: 105px;
-  }
-  100% {
-    background-position: 105px;
-  }
-}
-@-moz-keyframes shine {
-  0% {
-    background-position: 0;
-  }
-  60% {
-    background-position: 105px;
-  }
-  100% {
-    background-position: 105px;
-  }
-}
-@-webkit-keyframes shine {
-  0% {
-    background-position: 0;
-  }
-  60% {
-    background-position: 105px;
-  }
-  100% {
-    background-position: 105px;
-  }
-}
-@-o-keyframes shine {
-  0% {
-    background-position: 0;
-  }
-  60% {
-    background-position: 105px;
-  }
-  100% {
-    background-position: 105px;
-  }
-}
-</style>
