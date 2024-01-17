b3d08633c996ef3ab4465a157538007571c98117
inagamov <inagamov.tyoma@gmail.com>
2024-01-17 17:50:56 +0300

    quasar config lang no html attr rewrite

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -114,7 +114,11 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
-      config: {},
+      config: {
+        lang: {
+          noHtmlAttrs: true,
+        },
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
