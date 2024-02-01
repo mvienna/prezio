5e7f571cd7900a80c9547bfe58e398fa01d0cae8
inagamov <inagamov.tyoma@gmail.com>
2024-02-01 03:47:20 +0300

    text formatting shortcuts

--- frontend/src/stores/studio/actions/shortcuts.js
+++ frontend/src/stores/studio/actions/shortcuts.js
@@ -1,6 +1,7 @@
-import { handleSlideUpdate } from "stores/studio/actions/update";
-
 export function handleShortcuts(event) {
+  /*
+   * save
+   */
   if (event.ctrlKey || event.metaKey) {
     if (event.key === "s") {
       event.preventDefault();
@@ -12,12 +13,84 @@ export function handleShortcuts(event) {
   if (activeElement && activeElement.tagName === "INPUT") return;
   if (this.mode === this.MODE_OPTIONS.TEXT_EDITING) return;
 
+  /*
+   * delete
+   */
   if (event.key === "Delete" || event.key === "Backspace") {
     event.preventDefault();
     this.deleteNodes();
   }
 
   if (event.ctrlKey || event.metaKey) {
+    /*
+     * text formatting
+     */
+    if (
+      this.transformer.default
+        ?.nodes()
+        ?.filter((node) => node.getAttr("name") === this.MODE_OPTIONS.TEXT)
+        .length
+    ) {
+      // bold
+      if (event.key === "b") {
+        event.preventDefault();
+        this.text.fontStyle = (
+          this.text.fontStyle.includes("bold")
+            ? this.text.fontStyle.replace("bold", "")
+            : this.text.fontStyle.replace("normal", "") + " bold"
+        )
+          .replace(/\s+/g, " ")
+          .trim();
+        this.applyCustomization();
+        return;
+      }
+
+      // underline
+      if (event.key === "u") {
+        event.preventDefault();
+        this.text.textDecoration = (
+          this.text.textDecoration.includes("underline")
+            ? this.text.textDecoration.replace("underline", "")
+            : this.text.textDecoration + " underline"
+        )
+          .replace(/\s+/g, " ")
+          .trim();
+        this.applyCustomization();
+        return;
+      }
+
+      // italic
+      if (event.key === "i") {
+        event.preventDefault();
+        this.text.fontStyle = (
+          this.text.fontStyle.includes("italic")
+            ? this.text.fontStyle.replace("italic", "")
+            : this.text.fontStyle.replace("normal", "") + " italic"
+        )
+          .replace(/\s+/g, " ")
+          .trim();
+        this.applyCustomization();
+        return;
+      }
+
+      // strike-through
+      if (event.key === "x" && event.shiftKey) {
+        event.preventDefault();
+        this.text.textDecoration = (
+          this.text.textDecoration.includes("line-through")
+            ? this.text.textDecoration.replace("line-through", "")
+            : this.text.textDecoration + " line-through"
+        )
+          .replace(/\s+/g, " ")
+          .trim();
+        this.applyCustomization();
+        return;
+      }
+    }
+
+    /*
+     * z-index
+     */
     if (event.key === "ArrowDown") {
       event.preventDefault();
 
@@ -42,33 +115,41 @@ export function handleShortcuts(event) {
       } else {
         this.moveUp();
       }
+
+      return;
     }
 
     // copy
     if (event.key === "c") {
       event.preventDefault();
       this.copyNodes();
+      return;
     }
 
     // paste
     if (event.key === "v") {
       event.preventDefault();
       this.pasteNodes();
+      return;
     }
 
     // duplicate
     if (event.key === "d") {
       event.preventDefault();
       this.duplicateNodes();
+      return;
     }
 
     // cut
     if (event.key === "x") {
       event.preventDefault();
       this.cutNodes();
+      return;
     }
 
-    // undo & redo
+    /*
+     * undo & redo
+     */
     if (event.key === "z") {
       event.preventDefault();
 
@@ -80,11 +161,16 @@ export function handleShortcuts(event) {
       } else {
         this.undo();
       }
+
+      return;
     }
 
+    // redo
     if (event.key === "y") {
       event.preventDefault();
       this.redo();
+
+      return;
     }
   }
 }
