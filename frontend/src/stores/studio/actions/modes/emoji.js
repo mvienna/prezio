161eb4c3c602466ab011c3dad70d5e4e0830e273
inagamov <inagamov.tyoma@gmail.com>
2024-04-02 01:32:25 +0300

    emoji (text) to image

--- frontend/src/stores/studio/actions/modes/emoji.js
+++ frontend/src/stores/studio/actions/modes/emoji.js
@@ -14,7 +14,22 @@ export function addEmoji(emoji, size = 100) {
     name: this.MODE_OPTIONS.EMOJI,
   });
 
-  this.layers.default.add(emojiText);
+  // this.layers.default.add(emojiText);
+
+  emojiText.toImage({
+    callback: (img) => {
+      const image = new Konva.Image({
+        x: this.scene.width / 2 - size / 2,
+        y: this.scene.height / 2 - size / 2,
+        width: size,
+        height: size,
+        draggable: true,
+        name: this.MODE_OPTIONS.IMAGE,
+        image: img,
+      });
+      this.layers.default.add(image);
+    },
+  });
 
   this.handleSlideUpdate();
 }
